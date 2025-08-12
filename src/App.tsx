23997a3c69654c836d683609067bf7a8d8d909d0
Luke Sullivan <luke@sukelully.dev>
2025-08-12 19:37:52 +0100

    feat: add link to source code, add SEO tags - closes #60, closes #34

--- src/App.tsx
+++ src/App.tsx
@@ -22,32 +22,44 @@ function App() {
   const chimeWeather = weather ?? defaultWeather;
 
   return (
-    <main className="mx-auto flex min-h-screen max-w-4xl flex-col gap-6 px-4 py-10 sm:gap-4">
-      {weather && (
-        <div className="flex flex-grow flex-col dark:text-white">
-          <WeatherCard weather={weather} />
+    <>
+      <main className="mx-auto flex min-h-screen max-w-4xl flex-col gap-6 px-4 py-10 sm:gap-4">
+        {weather && (
+          <div className="flex flex-grow flex-col dark:text-white">
+            <WeatherCard weather={weather} />
+          </div>
+        )}
+
+        <div className="flex flex-grow items-center justify-center">
+          <ChimeCanvas weather={chimeWeather} />
         </div>
-      )}
 
-      <div className="flex flex-grow items-center justify-center">
-        <ChimeCanvas weather={chimeWeather} />
-      </div>
+        <div className="mt-auto">
+          <UICard
+            weather={weather}
+            // testWeather={testWeather}
+            location={location}
+            weatherLoading={weatherLoading}
+            weatherError={weatherError}
+            locationLoading={locationLoading}
+            locationError={locationError}
+            loadRandomCity={loadRandomCity}
+            handleLocationClick={handleLocationClick}
+            useExampleWeather={useExampleWeather}
+          />
+        </div>
+      </main>
 
-      <div className="mt-auto">
-        <UICard
-          weather={weather}
-          // testWeather={testWeather}
-          location={location}
-          weatherLoading={weatherLoading}
-          weatherError={weatherError}
-          locationLoading={locationLoading}
-          locationError={locationError}
-          loadRandomCity={loadRandomCity}
-          handleLocationClick={handleLocationClick}
-          useExampleWeather={useExampleWeather}
-        />
-      </div>
-    </main>
+      <a
+        href="https://github.com/sukelully/unwind-chimes"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="fixed bottom-2 left-2 z-50 rounded bg-gray-800 px-3 py-1 text-xs text-white opacity-70 transition-opacity hover:opacity-100 dark:bg-gray-200 dark:text-gray-900"
+      >
+        <i className="fa-brands fa-github mr-1" aria-hidden="true"></i>
+        View Source
+      </a>
+    </>
   );
 }
 
